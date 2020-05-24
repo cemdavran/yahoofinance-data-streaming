714e6353b65c20e5d8e4a17ea9066f3ae800e6d5
cemdavran <55216699+cemdavran@users.noreply.github.com>
2020-05-24 03:00:16 -0400

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,17 @@
+SELECT name as CompanyName, ts as Hour, high as HighestPrice, ts as HighestOccured
+
+
+FROM (SELECT name as Company, substring(ts,12,2) as hour1, MAX(high) as highprice 
+      FROM yahoo_finance_data GROUP BY name, substring(ts,12,2)) 
+
+firsttable 
+JOIN 
+(SELECT name, substring(ts,12,2) as hour2, high,ts FROM yahoo_finance_data) 
+secondtable 
+
+ON 
+firsttable.Company = secondtable.name 
+AND firsttable.hour1 =secondtable.hour2 
+AND firsttable.highprice = secondtable.high
+
+ORDER BY CompanyName, Hour;
